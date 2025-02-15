01f4aba3a1ab97aa734a28aa1e7b5810d3bd9173
bizbiz <qbizouard@gmail.com>
2025-02-15 14:08:54 +0100

    software version working

--- config_analyzer/app/routes/software_versions.py
+++ config_analyzer/app/routes/software_versions.py
@@ -4,11 +4,11 @@ from app.extensions import db
 
 software_versions_bp = Blueprint('software_versions', __name__)
 
-@software_versions_bp.route('/software_versions')
-def list_software_versions():
-    software_versions = SoftwareVersion.query.all()
-    softwares = Software.query.all()
-    return render_template('software_versions.html', software_versions=software_versions, softwares=softwares)
+@software_versions_bp.route('/software_versions/<int:software_id>')
+def list_software_versions(software_id):
+    software = Software.query.get_or_404(software_id)
+    software_versions = SoftwareVersion.query.filter_by(software_id=software_id).all()
+    return render_template('software_versions.html', software=software, software_versions=software_versions)
 
 @software_versions_bp.route('/software_versions/add', methods=['POST'])
 def add_software_version():
@@ -17,25 +17,26 @@ def add_software_version():
 
     if not software_id or not version:
         flash("Le logiciel et la version sont obligatoires.", "error")
-        return redirect(url_for('software_versions.list_software_versions'))
+        return redirect(url_for('software_versions.list_software_versions', software_id=software_id))
 
     new_software_version = SoftwareVersion(software_id=software_id, version=version)
     db.session.add(new_software_version)
     db.session.commit()
 
     flash("Version de logiciel ajoutée avec succès !", "success")
-    return redirect(url_for('software_versions.list_software_versions'))
+    return redirect(url_for('software_versions.list_software_versions', software_id=software_id))
 
 @software_versions_bp.route('/software_versions/edit/<int:software_version_id>', methods=['GET', 'POST'])
 def edit_software_version(software_version_id):
     software_version = SoftwareVersion.query.get_or_404(software_version_id)
+    software_id = software_version.software_id
     if request.method == 'POST':
         software_version.software_id = request.form['software_id']
         software_version.version = request.form['version']
 
         db.session.commit()
         flash("Version de logiciel modifiée avec succès !", "success")
-        return redirect(url_for('software_versions.list_software_versions'))
+        return redirect(url_for('software_versions.list_software_versions', software_id=software_id))
 
     softwares = Software.query.all()
     return render_template('edit_software_version.html', software_version=software_version, softwares=softwares)
@@ -43,7 +44,8 @@ def edit_software_version(software_version_id):
 @software_versions_bp.route('/software_versions/delete/<int:software_version_id>', methods=['GET'])
 def delete_software_version(software_version_id):
     software_version = SoftwareVersion.query.get_or_404(software_version_id)
+    software_id = software_version.software_id
     db.session.delete(software_version)
     db.session.commit()
     flash("Version de logiciel supprimée avec succès !", "success")
-    return redirect(url_for('software_versions.list_software_versions'))
+    return redirect(url_for('software_versions.list_software_versions', software_id=software_id))
