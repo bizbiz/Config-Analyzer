231b7e10f23e2d48c6f35b212e9ed8417fc0ea5d
bizbiz <qbizouard@gmail.com>
2025-02-16 01:30:53 +0100

    big commit almost everything is working

--- config_analyzer/app/routes/robot_models.py
+++ config_analyzer/app/routes/robot_models.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from app.models import RobotModel
+from app.models import RobotModel, Software
 from app.extensions import db
 
 robot_models_bp = Blueprint('robot_models', __name__)
@@ -7,7 +7,8 @@ robot_models_bp = Blueprint('robot_models', __name__)
 @robot_models_bp.route('/robot_models')
 def list_robot_models():
     robot_models = RobotModel.query.all()
-    return render_template('list/robot_models.html', robot_models=robot_models)
+    softwares = Software.query.all()
+    return render_template('list/robot_models.html', robot_models=robot_models, softwares=softwares)
 
 @robot_models_bp.route('/robot_models/<int:robot_model_id>/view')
 def view_robot_model(robot_model_id):
@@ -19,19 +20,21 @@ def add_robot_model():
     if request.method == 'POST':
         name = request.form.get('name')
         company = request.form.get('company')
-
+        software_id = request.form.get('software_id')
+        
         if not name:
             flash("Le nom du modèle de robot est obligatoire.", "error")
-            return redirect(url_for('robot_model.add_robot_model'))
-
-        new_robot_model = RobotModel(name=name, company=company)
+            return redirect(url_for('robot_models.add_robot_model'))
+        
+        new_robot_model = RobotModel(name=name, company=company, software_id=software_id)
         db.session.add(new_robot_model)
         db.session.commit()
-
+        
         flash("Modèle de robot ajouté avec succès !", "success")
-        return redirect(url_for('robot_model.list_robot_models'))
-
-    return render_template('add/robot_model.html')
+        return redirect(url_for('robot_models.list_robot_models'))
+    
+    softwares = Software.query.all()
+    return render_template('add/robot_model.html', softwares=softwares)
 
 @robot_models_bp.route('/robot_models/edit/<int:robot_model_id>', methods=['GET', 'POST'])
 def edit_robot_model(robot_model_id):
@@ -39,12 +42,14 @@ def edit_robot_model(robot_model_id):
     if request.method == 'POST':
         robot_model.name = request.form['name']
         robot_model.company = request.form['company']
-
+        robot_model.software_id = request.form['software_id']
+        
         db.session.commit()
         flash("Modèle de robot modifié avec succès !", "success")
-        return redirect(url_for('robot_model.list_robot_models'))
-
-    return render_template('edit/robot_model.html', robot_model=robot_model)
+        return redirect(url_for('robot_models.list_robot_models'))
+    
+    softwares = Software.query.all()
+    return render_template('edit/robot_model.html', robot_model=robot_model, softwares=softwares)
 
 @robot_models_bp.route('/robot_models/delete/<int:robot_model_id>', methods=['GET'])
 def delete_robot_model(robot_model_id):
@@ -52,4 +57,4 @@ def delete_robot_model(robot_model_id):
     db.session.delete(robot_model)
     db.session.commit()
     flash("Modèle de robot supprimé avec succès !", "success")
-    return redirect(url_for('robot_model.list_robot_models'))
+    return redirect(url_for('robot_models.list_robot_models'))
--- config_analyzer/app/routes/softwares.py
+++ config_analyzer/app/routes/softwares.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from app.models import Software
+from app.models import Software, RobotModel
 from app.extensions import db
 
 softwares_bp = Blueprint('softwares', __name__)
@@ -7,42 +7,55 @@ softwares_bp = Blueprint('softwares', __name__)
 @softwares_bp.route('/softwares')
 def list_softwares():
     softwares = Software.query.all()
-    return render_template('list/softwares.html', softwares=softwares)
-
-@softwares_bp.route('/softwares/<int:software_id>/view')
-def view_software(software_id):
-    software = Software.query.get_or_404(software_id)
-    return render_template('view/software.html', software=software)
+    robot_models = RobotModel.query.all()
+    return render_template('list/softwares.html', softwares=softwares, robot_models=robot_models)
 
 @softwares_bp.route('/softwares/add', methods=['GET', 'POST'])
 def add_software():
     if request.method == 'POST':
         name = request.form.get('name')
-
+        robot_model_ids = request.form.getlist('robot_model_ids')
+        
         if not name:
             flash("Le nom du logiciel est obligatoire.", "error")
             return redirect(url_for('softwares.add_software'))
-
+        
         new_software = Software(name=name)
         db.session.add(new_software)
         db.session.commit()
 
+        for robot_model_id in robot_model_ids:
+            robot_model = RobotModel.query.get(robot_model_id)
+            if robot_model:
+                new_software.robot_models.append(robot_model)
+        
+        db.session.commit()
+        
         flash("Logiciel ajouté avec succès !", "success")
         return redirect(url_for('softwares.list_softwares'))
-
-    return render_template('add/software.html')
+    
+    robot_models = RobotModel.query.all()
+    return render_template('add/software.html', robot_models=robot_models)
 
 @softwares_bp.route('/softwares/edit/<int:software_id>', methods=['GET', 'POST'])
 def edit_software(software_id):
     software = Software.query.get_or_404(software_id)
     if request.method == 'POST':
         software.name = request.form['name']
+        robot_model_ids = request.form.getlist('robot_model_ids')
 
+        software.robot_models.clear()
+        for robot_model_id in robot_model_ids:
+            robot_model = RobotModel.query.get(robot_model_id)
+            if robot_model:
+                software.robot_models.append(robot_model)
+        
         db.session.commit()
         flash("Logiciel modifié avec succès !", "success")
         return redirect(url_for('softwares.list_softwares'))
-
-    return render_template('edit/software.html', software=software)
+    
+    robot_models = RobotModel.query.all()
+    return render_template('edit/software.html', software=software, robot_models=robot_models)
 
 @softwares_bp.route('/softwares/delete/<int:software_id>', methods=['GET'])
 def delete_software(software_id):
